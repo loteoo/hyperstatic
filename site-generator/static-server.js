67c7d63b5f39d57460072f677e50b6ec07546f4d
Alexandre Lotte <alexlotte16@gmail.com>
2019-06-18 15:31:23 -0400

    fix static server

--- site-generator/static-server.js
+++ site-generator/static-server.js
@@ -3,56 +3,54 @@ var fs = require('fs');
 var path = require('path');
 
 http.createServer(function (request, response) {
-    console.log('request ', request.url);
 
-    var filePath = request.url;
-    if (filePath == '/') {
-      filePath = '/index.html';
+  console.log('request ', request.url);
+
+  var filePath = request.url;
+  if (filePath == '/') {
+    filePath = '/index.html';
+  }
+
+  filePath = path.join(__dirname, '..', 'dist', filePath)
+
+  var extname = String(path.extname(filePath)).toLowerCase();
+  var mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+    '.wav': 'audio/wav',
+    '.mp4': 'video/mp4',
+    '.woff': 'application/font-woff',
+    '.ttf': 'application/font-ttf',
+    '.eot': 'application/vnd.ms-fontobject',
+    '.otf': 'application/font-otf',
+    '.svg': 'application/image/svg+xml',
+    '.wasm': 'application/wasm'
+  };
+
+  var contentType = mimeTypes[extname] || 'application/octet-stream';
+
+  fs.readFile(filePath, function(error, content) {
+
+    // If 404
+    if (error) {
+
+      // return home page
+      var home = path.join(__dirname, '..', 'dist', 'index.html');
+      fs.readFile(home, function(error, content) {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.end(content, 'utf-8');
+      });
+
+    } else {
+      response.writeHead(200, { 'Content-Type': contentType });
+      response.end(content, 'utf-8');
     }
-
-    filePath = path.join(__dirname, '..', 'dist', filePath)
-
-
-    var extname = String(path.extname(filePath)).toLowerCase();
-    var mimeTypes = {
-        '.html': 'text/html',
-        '.js': 'text/javascript',
-        '.css': 'text/css',
-        '.json': 'application/json',
-        '.png': 'image/png',
-        '.jpg': 'image/jpg',
-        '.gif': 'image/gif',
-        '.wav': 'audio/wav',
-        '.mp4': 'video/mp4',
-        '.woff': 'application/font-woff',
-        '.ttf': 'application/font-ttf',
-        '.eot': 'application/vnd.ms-fontobject',
-        '.otf': 'application/font-otf',
-        '.svg': 'application/image/svg+xml',
-        '.wasm': 'application/wasm'
-    };
-
-    var contentType = mimeTypes[extname] || 'application/octet-stream';
-
-    fs.readFile(filePath, function(error, content) {
-        if (error) {
-            if(error.code == 'ENOENT') {
-                fs.readFile(path.join(__dirname, '..', 'dist', '/404.html'), function(error, content) {
-                    response.writeHead(404, { 'Content-Type': contentType });
-                    response.end(content, 'utf-8');
-                });
-            }
-            else {
-                response.writeHead(500);
-                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-                response.end();
-            }
-        }
-        else {
-            response.writeHead(200, { 'Content-Type': contentType });
-            response.end(content, 'utf-8');
-        }
-    });
+  });
 
 }).listen(8080);
 console.log('Server running at http://localhost:8080/');
