c43d8e600c1f7cb73a2a89e5e1c6beed6326633b
Alexandre Lotte <alexlotte16@gmail.com>
2019-07-19 19:58:17 -0400

    Better file not found handling in static server

--- src/createStaticServer.js
+++ src/createStaticServer.js
@@ -8,7 +8,7 @@ const createStaticServer = (port) => {
   return http.createServer((request, response) => {
     // Get req path without query string
     let reqPath = request.url.split('?')[0]
-
+    // console.log('req: ' + reqPath)
     if (reqPath === '/') {
       reqPath = '/index.html'
     }
@@ -17,9 +17,13 @@ const createStaticServer = (port) => {
       reqPath = reqPath + '/index.html'
     }
 
-    const filePath = path.join(__dirname, '../../../dist', reqPath)
+    let filePath = path.join(__dirname, '../../../dist', reqPath)
 
-    // console.log('request ', filePath)
+    // If not a file and not exist, use root index page
+    if (!reqPath.includes('.') && !fs.existsSync(filePath)) {
+      // console.log('Page does not exist. Returning home: ' + filePath)
+      filePath = path.join(__dirname, '../../../dist', '/index.html')
+    }
 
     const extname = String(path.extname(filePath)).toLowerCase()
     const mimeTypes = {
@@ -43,17 +47,11 @@ const createStaticServer = (port) => {
     const contentType = mimeTypes[extname] || 'application/octet-stream'
 
     fs.readFile(filePath, (error, content) => {
-      // If 404
       if (error) {
-        // return home page
-        const home = path.join(__dirname, '../../../dist/index.html')
-        fs.readFile(home, (error, content) => {
-          if (error) {
-            console.log(error)
-          }
-          response.writeHead(200, { 'Content-Type': 'text/html' })
-          response.end(content, 'utf-8')
-        })
+        console.log('File not found. Error: ')
+        console.log(error)
+        response.writeHead(404, { 'Content-Type': 'text/plain' })
+        response.write('404 Not found')
       } else {
         response.writeHead(200, { 'Content-Type': contentType })
         response.end(content, 'utf-8')
