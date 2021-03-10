92b619152985dd8decb15f213fdf5c153ad538d8
Alexandre Lotte <alexandre.lotte@osedea.com>
2021-03-09 21:27:23 -0500

    Export onRouteChanged sub

--- src/subscriptions/onRouteChanged.ts
+++ src/subscriptions/onRouteChanged.ts
@@ -6,7 +6,7 @@ import fx from '../utils/fx'
  */
 const onRouteChanged = fx((dispatch, action) => {
   const handleLocationChange = () => {
-    dispatch([action, window.location.pathname + window.location.search])
+    dispatch(action, window.location.pathname + window.location.search)
   }
   addEventListener('pushstate', handleLocationChange)
   addEventListener('popstate', handleLocationChange)
